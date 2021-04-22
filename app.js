5df5f9d9ebc22d0503d212bdbbc00798814b56c4
Payal Khattri <payal.cs18@nitp.ac.in>
2021-04-22 23:53:03 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const app=express()
+
+var cons = require('consolidate');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set("view engine","html")
+app.use('/public',express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.render('index.html')
+})
+app.get('*',(req,res) =>{
+    res.send('404 not found')
+})
+app.listen(3000,()=>{
+    console.log('Server Started')
+})
---
+++ public/js/quiz.js
@@ -0,0 +1,15 @@
+function quiz(){
+const quizQ=[
+    {
+        ques: "parsing of the program",
+        opt:{
+            a: "parsing of the program",
+            b: "the code generation",
+            c: "the lexical analysis of the program",
+            d: "dataflow analysis"
+        },
+        correct: "c"
+    }
+];
+console.log("yes")
+}
